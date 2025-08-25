bd4671a3022edd26613bd33b71a8734f590d171e
WisW <78122384+winsrewu@users.noreply.github.com>
2025-08-25 21:10:44 +0800

    feat: add -g for build scripts

--- app/testraw/build.sh
+++ app/testraw/build.sh
@@ -1,5 +1,5 @@
 set -e
 
-riscv32-unknown-elf-gcc -static -nostdlib -march=rv32i -mabi=ilp32 -T link.ld -o app.elf app.S
+riscv32-unknown-elf-gcc -g -static -nostdlib -march=rv32i -mabi=ilp32 -T link.ld -o app.elf app.S
 riscv32-unknown-elf-objcopy -F verilog app.elf app.mem
 riscv32-unknown-elf-objdump -S -d app.elf > app.lst
