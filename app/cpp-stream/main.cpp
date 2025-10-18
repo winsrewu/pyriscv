3c482fb388847ca58ae2801e13c5282cc9087e70
WisW <78122384+winsrewu@users.noreply.github.com>
2025-10-19 07:18:16 +0800

    fix: remove useless init code

--- app/cpp-stream/main.cpp
+++ app/cpp-stream/main.cpp
@@ -1,14 +1,7 @@
 #include <iostream>
 
-extern "C"
-{
-    void __libc_init_array(void);
-}
-
 int main()
 {
-    __libc_init_array();
-
     std::cout << "What's your name?" << std::endl;
     std::string name;
     std::cin >> name;
