4ded06e961835acc29d980bede157e0f2ded7f96
WisW <78122384+winsrewu@users.noreply.github.com>
2025-10-25 21:28:00 +0800

    fix: can't compile eliza due to gp

--- app/cpp-eliza/build.sh
+++ app/cpp-eliza/build.sh
@@ -1,5 +1,5 @@
 set -e
 
-riscv32-unknown-elf-g++ -g -O3 -std=c++20 -static -specs=nosys.specs -march=rv32i -mabi=ilp32 -DNDEBUG -T ../c-common/link.ld -o app.elf ../c-common/vectors.S ../c-common/syscalls.c main.cpp -lc -lgcc
+riscv32-unknown-elf-g++ -g -O3 -std=c++20 -static -specs=nosys.specs -march=rv32i -mabi=ilp32 -DNDEBUG -T ../c-common/gptype_b.ld -o app.elf ../c-common/vectors.S ../c-common/syscalls.c main.cpp -lc -lgcc
 riscv32-unknown-elf-objcopy -F verilog app.elf app.mem
 riscv32-unknown-elf-objdump -S -d app.elf > app.lst
