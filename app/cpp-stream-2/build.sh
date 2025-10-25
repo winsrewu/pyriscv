9b4bb615cf5297d482fdf25e9d48bdada1015313
WisW <78122384+winsrewu@users.noreply.github.com>
2025-10-25 19:34:32 +0800

    feat: two versions of link script and a new example

---
+++ app/cpp-stream-2/build.sh
@@ -0,0 +1,5 @@
+set -e
+
+riscv32-unknown-elf-g++ -g -O3 -static -specs=nosys.specs -march=rv32i -mabi=ilp32 -T ../c-common/gptype_b.ld -o app.elf ../c-common/vectors.S ../c-common/syscalls.c main.cpp -lc -lgcc
+riscv32-unknown-elf-objcopy -F verilog app.elf app.mem
+riscv32-unknown-elf-objdump -S -d app.elf > app.lst
