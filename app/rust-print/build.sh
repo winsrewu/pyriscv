a15b424ba0c810548a9faefaf118919785ceb383
WisW <78122384+winsrewu@users.noreply.github.com>
2026-05-23 19:49:52 +0800

    fix: set global pointer and memory relax correctly, remove uselss efforts on it before and changed memory structure & file format not recognized when obj-copy issue

--- app/rust-print/build.sh
+++ app/rust-print/build.sh
@@ -1,4 +1,4 @@
 set -e
 
 cargo build --release
-riscv32-unknown-elf-objcopy -F verilog target/riscv32i-unknown-none-elf/release/rust-print app.mem
+riscv32-unknown-elf-objcopy -O verilog target/riscv32i-unknown-none-elf/release/rust-print app.mem
